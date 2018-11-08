17115bede929650092b35ae1e354938b0a815852
Nathan Smith <nathan@nathansmith.io>
2018-11-08 15:22:56 -0800

    Initial Button (#2)

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { css } from 'react-emotion'
+
+/*
+types:
+"plus"
+"minus"
+"trash"
+"cook"
+*/
+
+class Button extends Component {
+  render() {
+    return (
+      <button
+        className={css`
+          color: red;
+          background: white;
+          border-radius: 50%;
+          box-shadow: 1px 0 20px 6px #eaeaea;
+          font-size: 24px;
+          font-weight: bold;
+          width: 70px;
+          height: 70px;
+          border: none;
+        `}
+        onClick={this.props.onClick}
+      >
+        +
+      </button>
+    )
+  }
+}
+
+export default Button
