6c7206be25ee0e144018cc7b5dd134fa6af56a5e
Nathan Smith <nathan@nathansmith.io>
2018-11-26 09:55:41 -0800

    woohoo

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import { css } from 'react-emotion'
-import '../assets/style.css'
 
 import shadows from '../global/shadows'
 import borderRadii from '../global/borderRadii'
 import fonts from '../global/fonts'
-import $ from 'jquery'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
@@ -15,129 +13,57 @@ class Card extends Component {
     selectMode: false,
   }
 
-  constructor(props) {
-    super(props)
-    this.parentRef = React.createRef()
-  }
-
-  toggleSelectMode = () => {
-    $(this.parentRef.current)
-      .find('.card-selection-overlay')
-      .toggleClass('select-mode')
-    this.setState({ selectMode: !this.state.selectMode })
-  }
-
-  toggleSelect = () => {
-    if (this.state.selectMode) {
-      $(this.parentRef.current)
-        .find('.card-selection-overlay')
-        .toggleClass('selected')
-    }
-  }
-
   render() {
     return (
       <div
-        ref={this.parentRef}
-        onClick={this.toggleSelect}
-        className={
-          'card ' +
-          css`
-            background: white;
-            box-shadow: ${shadows.default};
-            border-radius: ${borderRadii.small};
-            width: 100%;
-            height: 60px;
-            display: flex;
-            margin-bottom: 15px;
-            overflow: hidden;
-          `
-        }
+        className={css`
+          background: white;
+          box-shadow: ${shadows.default};
+          border-radius: ${borderRadii.small};
+          width: 100%;
+          height: 60px;
+          display: flex;
+          margin-bottom: 15px;
+          overflow: hidden;
+        `}
       >
         <div
-          className={
-            'card-image ' +
-            css`
+          className={css`
             background-image: url('${this.props.image}');
             background-size: 120%;
             background-position: 50% 50%;
             height: 100%;
             width: 90px;
-          `
-          }
+          `}
         >
           <div
-            className={
-              'card-selection-overlay ' +
-              css`
-                opacity: 0;
-                width: 100%;
-                height: 100%;
-                background: rgba(0, 0, 0, 0.4);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              `
-            }
-          >
-            <div
-              className={
-                'card-selection-circle ' +
-                css`
-                  width: 35px;
-                  height: 35px;
-                  border-radius: 50%;
-                  border: 3px solid white;
-                  background: rgba(0, 0, 0, 0);
-                  display: flex;
-                  align-items: center;
-                  justify-content: center;
-                  color: white;
-                  cursor: pointer;
-                `
-              }
-            >
-              <FontAwesomeIcon icon={faCheck} />
-            </div>
-          </div>
-        </div>
-        <div
-          className={
-            'card-text ' +
-            css`
-              padding: 16px;
-              font-size: 14px;
-              font-family: ${fonts.header};
-              font-weight: bold;
+            className={css`
+              opacity: 0;
+              width: 100%;
+              height: 100%;
+              background: rgba(0, 0, 0, 0.4);
               display: flex;
               align-items: center;
-              flex-grow: 1;
-              text-overflow: ellipsis;
-              overflow: hidden;
-              white-space: nowrap;
-            `
-          }
+              justify-content: center;
+            `}
+          />
+        </div>
+        <div
+          className={css`
+            padding: 16px;
+            font-size: 14px;
+            font-family: ${fonts.header};
+            font-weight: bold;
+            display: flex;
+            align-items: center;
+            flex-grow: 1;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+          `}
         >
           {this.props.text}
         </div>
-        {this.props.quantity && (
-          <div
-            className={
-              'card-quantity ' +
-              css`
-                height: 100%;
-                width: auto;
-                padding: 20px;
-                display: flex;
-                align-items: center;
-                font-family: ${fonts.header};
-                font-weight: bold;
-              `
-            }
-          >
-            {this.props.quantity}
-          </div>
-        )}
       </div>
     )
   }
--- src/pages/IngredientSearchResults/index.js
+++ src/pages/IngredientSearchResults/index.js
@@ -3,12 +3,36 @@ import { css } from 'react-emotion'
 
 import Page from '../../components/Page'
 import Header from '../../components/Header'
+import Card from '../../components/Card'
 
 class IngredientSearchResults extends Component {
+  state = {
+    results: [],
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `https://food-api-proxy.herokuapp.com/ingredient-search?q=${
+        this.props.ingredient
+      }`
+    )
+    const data = await res.json()
+    this.setState({ results: data })
+  }
+
   render() {
     return (
       <Page backButton={true}>
-        <Header>Results similar to “{this.props.searchTerm}”</Header>
+        <Header>Results similar to “{this.props.ingredient}”</Header>
+        {this.state.results.map((result, i) => (
+          <Card
+            text={result.name}
+            image={`https://spoonacular.com/cdn/ingredients_100x100/${
+              result.image
+            }`}
+            key={i}
+          />
+        ))}
       </Page>
     )
   }
